596ac98d39c96131b986a0fef22bc88e96e88bf4
higorspinto <higorspinto@gmail.com>
2020-03-27 08:56:59 -0300

    [feature][s] - Cookie control configuration. ref #88

--- ckanext/nhs/fanstatic/js/civic_cookies.js
+++ ckanext/nhs/fanstatic/js/civic_cookies.js
@@ -1,15 +1,18 @@
 ckan.module('nhs_civic_cookies', function ($) {
     return {
       initialize: function () {
-        console.log("I've been initialized for element: ", this.el);
         var config = {
             apiKey: '925424c91d627482e30e3c6953e2c61a6ac7ee8d',
             product: 'COMMUNITY',
             position: 'RIGHT',
-            theme: 'LIGHT'
+            theme: 'LIGHT',
+            initialState: 'OPEN',
+            necessaryCookies: ['ckan','auth_tkt'],
         };
           
         CookieControl.load( config );
     }
     };
 });
+
+    
