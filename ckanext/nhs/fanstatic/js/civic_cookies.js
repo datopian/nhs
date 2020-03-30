ae3f904c73cf4fd49237b8027e944d3478ab30a8
higorspinto <higorspinto@gmail.com>
2020-03-30 14:33:33 -0300

    [fix][xs] - Changing Cookie Control page info. ref #88

--- ckanext/nhs/fanstatic/js/civic_cookies.js
+++ ckanext/nhs/fanstatic/js/civic_cookies.js
@@ -11,7 +11,15 @@ ckan.module('nhs_civic_cookies', function ($) {
             necessaryCookies: ['ckan','1P_JAR', 'fldt', 'auth_tkt'],
             encodeCookie: true,
             notifyOnce: true,
-            rejectButton: false
+            rejectButton: false,
+            text: {
+              title: 'Our use of cookies',
+              intro: 'We use necessary cookies to make our site work. For more detailed information about the cookies we use, see our Cookies page.',
+              accept: 'Save and close',
+              acceptSettings: 'Save and close',
+              necessaryTitle: 'Necessary cookies',
+              necessaryDescription: 'Necessary cookies enable core functionality such as security, network management, and accessibility. You may disable these by changing your browser settings, but this may affect how the website functions.',
+            }
         };
           
         CookieControl.load( config );
