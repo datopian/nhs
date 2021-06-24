cb28cf7d16a5fe228d4084933d127e19a60fd128
ismailshahzad <ismailshahzadmirza@gmail.com>
2021-06-24 15:15:55 +0500

    [actions.py] Refactors the action

--- ckanext/nhs/logic/actions.py
+++ ckanext/nhs/logic/actions.py
@@ -6,14 +6,20 @@ from ckan.plugins import toolkit
 from ckan.common import config, request
 import ckan.logic as logic
 _check_access = logic.check_access
+import json
 
 import datetime
 import logging
 log = logging.getLogger(__name__)
 import subprocess
+
 @toolkit.side_effect_free
 def get_cloud_egress(context, data_dict):
     log.debug(data_dict)
     _check_access('egress_gcp_fetch',context,data_dict)
-    res = subprocess.check_output(['python3', '/srv/app/gcp_storage_egress.py'])
-    return res
+    if ('start_time' and 'end_time') not in data_dict:
+        return u'Please provide start_time and endtime'
+    start_time = data_dict.get('start_time')
+    end_time = data_dict.get('end_time')
+    result = subprocess.check_output(['python3', '/srv/app/gcp_storage_egress.py',start_time,end_time])
+    return result.strip()
