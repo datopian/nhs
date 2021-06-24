bd218af36dbc0c787f7b1c5a3bf596aac0e2ed16
ismailshahzad <ismailshahzadmirza@gmail.com>
2021-06-24 15:55:18 +0500

    [actions.py] Refactors the action returned datatype

--- ckanext/nhs/logic/actions.py
+++ ckanext/nhs/logic/actions.py
@@ -15,11 +15,15 @@ import subprocess
 
 @toolkit.side_effect_free
 def get_cloud_egress(context, data_dict):
-    log.debug(data_dict)
+    '''
+    Fetches egress data from gcp
+    '''
     _check_access('egress_gcp_fetch',context,data_dict)
     if ('start_time' and 'end_time') not in data_dict:
         return u'Please provide start_time and endtime'
     start_time = data_dict.get('start_time')
     end_time = data_dict.get('end_time')
     result = subprocess.check_output(['python3', '/srv/app/gcp_storage_egress.py',start_time,end_time])
-    return result.strip()
+    result = json.dumps(result.strip())
+    result = json.loads(result)
+    return result
